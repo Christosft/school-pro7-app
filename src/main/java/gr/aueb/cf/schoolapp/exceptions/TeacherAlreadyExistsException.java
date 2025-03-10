484a1f19c54da702afbf310cfd2cca880e79e484
Chris Ftoulis <christosftoulis3@gmail.com>
2025-03-10 21:32:22 +0100

    feat: create mapper services

---
+++ src/main/java/gr/aueb/cf/schoolapp/exceptions/TeacherAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.schoolapp.exceptions;
+
+public class TeacherAlreadyExistsException extends Exception {
+
+    public TeacherAlreadyExistsException(String message) {
+
+      super(message);
+    }
+}
